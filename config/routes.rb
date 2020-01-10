8c6c128f126bfe8ec9c6bec409447af5ebe7e49b
github username <github email address>
2020-01-10 15:40:20 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Create a route that is mapped to a students' controller index action, 
+  # the resulting route should be /students
+
+  # get 'posts/:id', to: 'posts#show'
+
+  # get '/students' to: 'students#index'
+  resources :students, only: :index 
+
 end
